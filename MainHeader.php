359cb45021e7b5a8eb720c013924f1520a0af9a2
Dobri991 <59454884+Dobri991@users.noreply.github.com>
2020-01-02 23:27:11 +0200

    Add files via upload

---
+++ MainHeader.php
@@ -0,0 +1,32 @@
+  <div id = "boxes">  
+    <div id = "leftbox"> 
+            <p id = "company-name">Company Name</p>
+        </div>  
+          
+<div id = "middlebox"> 
+        <div class="wrapper">
+            <input type="text" class="input" placeholder="What are you looking for?">
+        <div class="searchbtn"><i class="fas fa-search"></i></div>
+    </div> 
+</div>
+<!--3 boxes-->
+<div id = "rightbox"> 
+    <div class="navbar-toolbox">
+<div class = "icons">
+    <div class = "Log-in">
+        <div class="toggle-button">Login</div> 
+    </div>
+</div>
+<div class = "icons">
+        <div class = "Sign-up">
+            <div class="toggle-button1">Sign up</div> 
+        </div>
+    </div>
+    <div class = "icons">
+            <div class = "My-Cart">
+                <div class="toggle-button2">My Cart</div> 
+            </div>
+        </div>
+      </div> 
+    </div> 
+</div>
