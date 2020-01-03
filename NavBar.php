01b84446447cb8372dcb438d402cca1e9cb1f19f
Dobri991 <59454884+Dobri991@users.noreply.github.com>
2020-01-03 14:02:22 +0200

    Add files via upload

---
+++ NavBar.php
@@ -0,0 +1,9 @@
+<!--Navigation bar-->
+<nav>
+    <ul>
+        <li><a href="#">Activities</a></li>
+        <li><a href="#">Parks</a></li>
+        <li><a href="#">Shops</a></li>
+        <li><a href="#">Events</a></li>
+    </ul>
+</nav>
