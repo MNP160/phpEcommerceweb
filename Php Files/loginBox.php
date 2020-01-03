1c9f74d64af1893fbaf0de0b5462f600ad9ce5ea
Dobri991 <59454884+Dobri991@users.noreply.github.com>
2020-01-03 12:44:28 +0200

    Add files via upload

---
+++ Php Files/loginBox.php
@@ -0,0 +1,12 @@
+<!--Login Form-->
+<div class= "login-box">
+    <p id = "register-heading">Hello again!</p>
+        <div class = "button-box">
+            <form action="#" class = "input-group" method="GET">
+            <input type="text" class="input-field"  placeholder="E-mail" required name="E-mail">
+            <input type="password" class="input-field" placeholder="Password" required name="Password"> 
+                        
+           <input type="submit" class="Login-button" value="Login">
+        </form>
+    </div>
+</div>
