c2090d49cea85de94ec4c04c8320c974231df3a9
Mladen <mpenev13@gmail.com>
2020-01-09 15:54:24 +0200

    fixing repo structure

--- Php Files/NavBar.php
+++
@@ -1,9 +0,0 @@
-<!--Navigation bar-->
-<nav>
-    <ul>
-        <li><a href="#">Activities</a></li>
-        <li><a href="#">Parks</a></li>
-        <li><a href="#">Shops</a></li>
-        <li><a href="#">Events</a></li>
-    </ul>
-</nav>
